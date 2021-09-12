a4ee2b660eece5af7375c4821077cc27f1be3f6c
Raul Tierno <raultm@gmail.com>
2021-09-12 22:27:41 +0200

    Spec 002 - Basic SW - Organize and extends tests to existent but not cached pages

--- cypress/integration/002_register_sw.spec.js
+++ cypress/integration/002_register_sw.spec.js
@@ -10,57 +10,85 @@
 
 
 
-console.log(Cypress.goOnline)
-  // since we are using Chrome debugger protocol API
-  // we should only run these tests when NOT in Firefox browser
-  // see https://on.cypress.io/configuration#Test-Configuration
-  describe('register service workers', { browser: '!firefox' }, () => {
-    // the application is making request to this url
-    const folder = '/002_register_sw/index.html'
+// since we are using Chrome debugger protocol API
+// we should only run these tests when NOT in Firefox browser
+// see https://on.cypress.io/configuration#Test-Configuration
+describe('basic service worker', { browser: '!firefox' }, () => {
+  // the application is making request to this url
+  const folder = '/002_register_sw/index.html'
   
-    // make sure we get back online, even if a test fails
-    // otherwise the Cypress can lose the browser connection
-    beforeEach(() => Cypress.goOnline() )
-    afterEach(() => Cypress.goOnline() )
+  // make sure we get back online, even if a test fails
+  // otherwise the Cypress can lose the browser connection
+  beforeEach(() => Cypress.goOnline() )
+  afterEach(() => Cypress.goOnline() )
+
+  it('shows sw status', () => {
+    cy.visit(folder)
+    cy.contains('#sw-status', 'SW not registered')
+  })
   
-    it('shows sw status', () => {
-      cy.visit(folder)
-      cy.contains('#sw-status', 'SW not registered')
-    })
-  
-    it('register sw updates status', () => {
+  describe('register service worker - hijack fetch events(online/offline) and serves only cached pages', () => {
+
+    beforeEach(() => {
       cy.visit(folder)
       cy.get('#register').click()
+      // Need to wait for status confirmation
       cy.contains('#sw-status', 'SW registered')
     })
-  
-    it('can load if sw registered, offline mode and try to visit', () => {
-      cy.visit(folder)
-      cy.contains('#sw-status', 'SW not registered')
-      cy.get('#register').click().wait(1000)
-      
-      Cypress.goOffline()
-      Cypress.assertOffline()
-      
-      cy.get('#fetch-url').clear().type('index.html')
-      cy.get('#fetch').click()
-      cy.contains('#fetch-response', '200')
-      
-      
-      Cypress.goOnline()
-      Cypress.assertOnline()
+    //afterEach(() =>  )
+
+    it('sw status reflects registration', () => {
+      cy.contains('#sw-status', 'SW registered')
     })
 
-    it('show 404 if sw registered, offline mode and try to visit non existing page', () => {
-      cy.visit(folder)
-      cy.contains('#sw-status', 'SW not registered')
-      cy.get('#register').click().wait(1000)
-      
-      Cypress.goOffline()
-      Cypress.assertOffline()
+    describe('offline mode', () => {
+      beforeEach(() => {
+        Cypress.goOffline()
+        Cypress.assertOffline()
+      })
 
-      cy.get('#fetch-url').clear().type('non-existent.php')
-      cy.get('#fetch').click()
-      cy.contains('#fetch-response', '404')
+      it('can load cached page', () => {
+        cy.get('#fetch-url').clear().type('index.html')
+        cy.get('#fetch').click()
+        cy.contains('#fetch-response', '200')
+      })
+
+      it('can not load non cached page', () => {
+        cy.get('#fetch-url').clear().type('test.html').wait(250)
+        cy.get('#fetch').click()
+        cy.contains('#fetch-response', '404')
+      })
+
+      it('can not load non existent', () => {
+        cy.get('#fetch-url').clear().type('non-existent.php').wait(250)
+        cy.get('#fetch').click()
+        cy.contains('#fetch-response', '404')
+      })
+    })
+
+    describe('online mode', () => {
+      beforeEach(() => {
+        Cypress.goOnline()
+        Cypress.assertOnline()
+      })
+
+      it('can load cached page', () => {
+        cy.get('#fetch-url').clear().type('index.html')
+        cy.get('#fetch').click()
+        cy.contains('#fetch-response', '200')
+      })
+
+      it('can not load non cached page', () => {
+        cy.get('#fetch-url').clear().type('test.html').wait(500)
+        cy.get('#fetch').click()
+        cy.contains('#fetch-response', '404')
+      })
+
+      it('can not load non existent', () => {
+        cy.get('#fetch-url').clear().type('non-existent.php').wait(250)
+        cy.get('#fetch').click()
+        cy.contains('#fetch-response', '404')
+      })
     })
   })
+})
